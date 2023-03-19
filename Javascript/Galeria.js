bf1a0b4d5a2469ad2ebac2ac8d4d4a6cd529e80e
Mobikaa <90673580+Mobikaa@users.noreply.github.com>
2023-03-19 13:19:32 +0100

    Add files via upload

---
+++ Javascript/Galeria.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.column img').forEach(image=>{
+    image.onclick = () => {
+        document.querySelector('.popup-img').style.display='block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+    }        
+});
+document.querySelector('.popup-img span').onclick = () => {
+    document.querySelector('.popup-img').style.display='none';
+}
